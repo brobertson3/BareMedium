85a5658935b283b5e45690c66304a566d2f70d53
brobertson3 <brobertson3@users.noreply.github.com>
2016-07-10 23:52:19 -0400

    Alot of different changes
    Check the diff lol. Added some JS to change the color of the background for each thumbnail, as well as overlay the skills for each project over the thumbnail. Introduced placeholders for upcoming projects. Modal box now pops up when clicking the read more button. Changed the Contact Me section so that it's centered. All around wellness changes for the responsiveness of the site. I think that's it.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	/* On hover - remove the hidden class and blurred_image to show the 'The Real Me' 
-	   text and clear profile picture*/
+	   text and clear profile picture. */
 	$('.main_image_div').hover(function () {
 		$('.main_image').removeClass('blurred_image');
 		$('.real_me').removeClass('hidden');
@@ -17,8 +17,44 @@ $(document).ready(function () {
 		$(this).append('<span class="sr-only">(current)</span>');
 	});
 
-	/* TODO
-		1. On hover give the thumbnail a red/blue overlay.
-		2. Show the skills used in it in a circle around the picture 
-		with either a red/blue border.*/
-});
+	/*	1. On hover give the thumbnail a red/blue overlay.
+		2. Show the skills used in the picture with either a red/blue border. */
+	$('div.thumbnail, div.project_skill').hover(function (event) {
+		// $(this).children().removeClass('hidden');
+		/* Kind of repetitive but ensures that it's the target that triggered
+		   event. */
+		/* This shows the skills on top of the div. */
+		$(this).parent().children('.project_skill').removeClass('hidden');
+
+		/* Find the index of the chosen element based on the grandparent (outermost div). */
+		var index = $(this).parent().parent().index();
+		/* If the index is even, then make the background red.
+		 * If odd then make the background blue.
+		 * If upcoming work don't change the background color. */
+		if ($(this).parent().hasClass('dashed_border')) { 
+			// Do nothing for upcoming work
+		} else if(index % 2 === 0) { //even
+			$(this).parent().children('.thumbnail').css('backgroundColor', '#ff0000');
+		} else { //odd
+			$(this).parent().children('.thumbnail').css('backgroundColor', '#0000ff');
+		}
+	}, function () {
+		/* Add the hidden class back so that the skills are hidden when not hovering. */
+		$(this).parent().children('.project_skill').addClass('hidden');
+		$(this).parent().children('.thumbnail').css('backgroundColor', 'transparent');
+	});
+
+	/* Change the size and color of the icons on hover. */
+	$('.fa.fa-fw').hover(function () {
+		$(this).removeClass('fa-2x');
+		$(this).addClass('fa-4x');
+		$(this).css("color", "red");
+	}, function () {
+		$(this).removeClass('fa-4x');
+		$(this).addClass('fa-2x');
+		$(this).css("color", "inherit");
+	});
+
+	/* TODO On click of #read_more we need to pop up a dialog box of some sort
+	   to display the rest of the contents of the description. */
+});	
